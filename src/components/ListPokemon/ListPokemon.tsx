7e5f124b14a380a1325b5c5529ae9f1d9717d733
Victor Araújo <victor.b_@hotmail.com>
2024-02-05 12:46:33 +0000

    feat: Refactor ListPokemon styling and add Hexagon pattern to ListPokemon

--- src/components/ListPokemon/ListPokemon.tsx
+++ src/components/ListPokemon/ListPokemon.tsx
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import { PokemonList } from "../../types/types";
+import Hexagon from "../../public/pattern/hexagon7.svg";
 
 interface ListPokemonProps {
   data: { pokemon_v2_pokemon: PokemonList[] };
@@ -11,7 +12,10 @@ export const ListPokemon = ({ data }: ListPokemonProps) => {
       {data.pokemon_v2_pokemon.map((pokemon: PokemonList) => (
         <div
           key={pokemon.id}
-          className="flex flex-col items-center bg-amber-100 p-4 rounded w-[128px]"
+          className={`flex flex-col items-center bg-rose-700
+          border-2 border-yellow-500 p-4 w-[128px] 
+          hover:bg-rose-600 hover:border-amber-200 hover:shadow-lg`}
+          style={{ backgroundImage: `url(${Hexagon.src})` }}
         >
           <h1>{pokemon.name}</h1>
           <h2># {pokemon.id}</h2>
@@ -30,7 +34,7 @@ export const ListPokemon = ({ data }: ListPokemonProps) => {
               <li
                 key={type.pokemon_v2_type.id}
                 className={`text-sm rounded ${type.pokemon_v2_type.name}
-              p-1 mb-1 flex justify-center items-center text-white`}
+              p-1 mb-1 flex justify-center items-center text-white text-zinc-100`}
               >
                 {type.pokemon_v2_type.name}
               </li>
